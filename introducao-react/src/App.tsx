3449bbdf4ce02874d2c1c3193e5cde2584b0325c
jose.ccna <jose.ccna@gmail.com>
2026-04-27 21:19:32 -0300

    primeiro commit

---
+++ introducao-react/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App(){
+
+  return(
+    <>
+      <h1>Diego</h1>
+      <h2>Desenvolvedor de Software</h2>
+
+    </> // Fragmento vazio para evitar a necessidade de uma div extra
+  );
+}
+
+export default App;
